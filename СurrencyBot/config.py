db5165dd17e692d9ec2849a907fc50cb0c87f4c3
DenisZ82 <148885923+DenisZ82@users.noreply.github.com>
2023-11-30 21:22:40 +1000

    Add files via upload

---
+++ СurrencyBot/config.py
@@ -0,0 +1,12 @@
+TOKEN = '6371282786:AAGG4jhTdMNnVi1pUlnPYd3THzpb1rD2SwE'
+
+keys = {
+    'доллар': 'USD',
+    'Доллар': 'USD',
+    'евро': 'EUR',
+    'Евро': 'EUR',
+    'юань': 'CNY',
+    'Юань': 'CNY',
+    'рубль': 'RUB',
+    'Рубль': 'RUB',
+}
